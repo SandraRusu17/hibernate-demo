413065fe6feae08b6a41aeaf2d21d805dfdf2d6a
SandraRusu <sandra.rusu.ext@extendaretail.com>
2022-05-26 10:33:15 +0300

    Creating and Saving Java Objects

--- src/main/java/com/luv2code/hibernate/demo/CreateStudentDemo.java
+++ src/main/java/com/luv2code/hibernate/demo/CreateStudentDemo.java
@@ -1,6 +1,7 @@
 package com.luv2code.hibernate.demo;
 
 import com.luv2code.hibernate.demo.entity.Student;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -14,6 +15,29 @@ public class CreateStudentDemo {
                                 .buildSessionFactory();
 
         // create session
+        Session session = factory.getCurrentSession();
 
+        try {
+            // use the session object to save the Java object
+
+            // create student object
+            System.out.println("Creating a new student object ...");
+            Student tempStudent = new Student("Sandra", "Rusu", "sandra.rusu@gmail.com");
+
+            // start a transaction
+            session.beginTransaction();
+
+            // save the student
+            System.out.println("Saving the student...");
+            session.save(tempStudent);
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done !");
+
+        }finally {
+            factory.close();
+        }
     }
 }
